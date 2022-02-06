28b8c879d1822e142bc5977a6b8c4e30dc8d8d64
Vladislav <vladlipchey05@gmail.com>
2022-02-06 19:58:39 +0200

    feat: all done

--- main.js
+++ main.js
@@ -1,37 +1,70 @@
 const playPause=document.querySelector(".play-pause");
-const audio=document.querySelector('.audio');
-const forward=document.querySelector('.forward');
-const backward=document.querySelector('.backward');
-const songs=["Beyonce.mp3","DuaLipa.mp3"]
+audio=document.querySelector('.audio');
+forward=document.querySelector('.forward');
+backward=document.querySelector('.backward');
+coverImg=document.querySelector('.coverImg');
+background=document.querySelector('.background');
+singer=document.querySelector('.singer');
+progressContainer=document.querySelector('.progress-bar');
+song_Name=document.querySelector(".songname");
+progressbar=document.querySelector('.progressbar')
+const songs=["Beyonce.mp3","DuaLipa.mp3"];
+const covers=["cover1.png", "cover2.png"]
+const singerName=["Beyonce","Dua Lipa"];
+const songName=["Don`t Hurt Yoursef", "Dont Start Now"];
 let nowPlaying=0;
-
+audio.addEventListener("timeupdate", progress);
+progressContainer.addEventListener('click',currentProgress)
 audio.src="assets/audio/" + songs[nowPlaying];
+// Функция для начала проигрывания песни
 function play(){
     audio.play();
     playPause.src="assets/svg/pause.png";
     playPause.onclick=pause;
-}
+}// Пауза
 function pause(){
     audio.pause();
     playPause.src="assets/svg/play.png";
     playPause.onclick=play;
-}
+}// Кнопка "следующая песня"
 function nextSong(){
     nowPlaying=nowPlaying+1;
     if(nowPlaying>1){
         nowPlaying=0;
     }
+    singer.innerHTML= singerName[nowPlaying];
+    song_Name.innerHTML=songName[nowPlaying];
+    coverImg.src="assets/img/"+covers[nowPlaying];
+    background.src="assets/img/"+covers[nowPlaying];
     audio.src="assets/audio/" + songs[nowPlaying];
     play();
-}
+}// Кнопка "предыдущая песня"
 function previousSong(){
     nowPlaying=nowPlaying-1;
     if(nowPlaying<0){
         nowPlaying=1;
     }
+    singer.innerHTML= singerName[nowPlaying];
+    song_Name.innerHTML=songName[nowPlaying];
+    background.src="assets/img/"+covers[nowPlaying];
+    coverImg.src="assets/img/"+covers[nowPlaying];
     audio.src="assets/audio/" + songs[nowPlaying];
     play();
 }
+function progress(start){
+    const{duration,currentTime}=start.srcElement;
+    const thisProgres=(currentTime/duration)*100;
+    progressbar.style.width=`${thisProgres}%`
+}
+//Перемотка песни
+function currentProgress(e){
+    width=this.clientWidth;
+    clickHere=e.offsetX;
+    dur=audio.duration;
+    audio.currentTime=(clickHere/width)*dur;
+}
+//Автопроигрывание
+audio.addEventListener('ended', nextSong)
 playPause.onclick=play;
 forward.onclick=nextSong;
 backward.onclick=previousSong;
